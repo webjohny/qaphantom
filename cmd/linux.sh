05da7fb88a66c98e26e0646cb4d1cbed373e6c15
webjohny <webtoolteam@gmail.com>
2021-03-11 13:33:54 +0200

    master, added servers with qa

--- cmd/linux.sh
+++ cmd/linux.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
 # shellcheck disable=SC2034
-IP_ADDR="45.90.35.231"
+#IP_ADDR="45.90.35.231"
+#IP_ADDR="213.139.209.79"
+#IP_ADDR="45.67.57.4"
+#IP_ADDR="45.67.59.68"
+IP_ADDR="193.200.74.67"
+PASS="Ghjcnjgfhjkm1!"
 
 cd ../
 export GOOS=linux
 go build .
 export GOOS=windows
 
-ssh -i "~/.ssh/id_rsa" "root@45.90.35.231" "service qaphantom stop"
-scp -r -i "~/.ssh/id_rsa" qaphantom "root@45.90.35.231:/var/www/html"
-ssh -i "~/.ssh/id_rsa" "root@45.90.35.231" "service qaphantom restart"
+ssh -i "~/.ssh/id_rsa" "root@$IP_ADDR" "service qaphantom stop"
+scp -r -i "~/.ssh/id_rsa" qaphantom "root@$IP_ADDR:/var/www/html"
+ssh -i "~/.ssh/id_rsa" "root@$IP_ADDR" "service qaphantom restart"
